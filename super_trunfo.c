f75d70b72cd33cab65106107c8aaf534a1582147
Felipe Moura Costa <202501054846@alunos.estacio.br>
2025-03-26 19:24:46 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
 
+                             // criando uma estrutura para armazenar os dados da carta
+
+    int main(){
+        char estado;            // Letra do estado (A-H)
+        char codigo[5];         // Código da carta (ex: A01)
+        char NomeDaCidade[20];  // Nome da cidade
+        int populacao;          // População
+        float area;             // Área em km²
+        float pib;              // Produto Interno Bruto 
+        int turisticos;         // Número de pontos turísticos
+
+    printf("Digite o nome do Estado:(A-H) \n");
+    scanf("%s", &estado);
+    printf("Estado: %s\n", estado);
+    }
